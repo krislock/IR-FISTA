21f382e86383dd6e569b84674ab923d5c33807d6
Nathan Krislock <nkrislock@niu.edu>
2019-12-28 00:41:08 -0600

    Updated runtests.jl to print rp and rd

--- src/runtests.jl
+++ src/runtests.jl
@@ -3,7 +3,7 @@ using Plots, LaTeXStrings, Printf
 include("runfile.jl")
 
 
-function runtests(n, γ, kmax, f_calls_limit)
+function runtests(n, γ; f_calls_limit=2000)
     U, H, ncm = genprob(n, γ, f_calls_limit=f_calls_limit)
 
     J = Symmetric(ones(n,n))
@@ -11,28 +11,32 @@ function runtests(n, γ, kmax, f_calls_limit)
     Xold = copy(ncm.Xold)
     y = copy(ncm.res.y)
 
-    t1 = @elapsed success =
-        ncm(U, H, method=:IAPG,
-            kmax=kmax,
-            useXold=true,
-            f_calls_limit=f_calls_limit)
+    tol = 1e-1
+    t1 = @elapsed success = ncm(U, H, method=:IAPG,
+                                useXold=true,
+                                f_calls_limit=f_calls_limit)
     fgcount = ncm.res.fgcountRef[]
     r1 = ncm.res.resvals[1:fgcount]
-    @printf("%4d %6.2f %6d %8s %8d %10.2e %8.2f\n",
-            n, γ, kmax, "IAPG", fgcount, r1[end], t1)
+    rp = ncm.res.rpRef[]
+    rd = ncm.res.rdRef[]
+    fval = ncm.res.fvals[fgcount]
+    @printf("%4d %6.2f %8s %8d %10.2e %10.2e %16.8e %8.2f\n",
+            n, γ, "IAPG", fgcount, rp, rd, fval, t1)
 
     tol = r1[end]
     ncm.Xold .= Xold
     ncm.res.y .= y
-    t2 = @elapsed success =
-        ncm(U, H, method=:IR, τ=0.95,
-            tol=tol,
-            useXold=true,
-            f_calls_limit=f_calls_limit)
+    t2 = @elapsed success = ncm(U, H, method=:IR, τ=0.95,
+                                tol=tol,
+                                useXold=true,
+                                f_calls_limit=f_calls_limit)
     fgcount = ncm.res.fgcountRef[]
     r2 = ncm.res.resvals[1:fgcount]
-    @printf("%4d %6.2f %6d %8s %8d %10.2e %8.2f\n",
-            n, γ, NaN, "IR", fgcount, r2[end], t2)
+    rp = ncm.res.rpRef[]
+    rd = ncm.res.rdRef[]
+    fval = ncm.res.fvals[fgcount]
+    @printf("%4d %6.2f %8s %8d %10.2e %10.2e %16.8e %8.2f\n",
+            n, γ, "IR", fgcount, rp, rd, fval, t2)
 
     return r1, r2
 end
@@ -49,8 +53,8 @@ function makeplot(r1, r2)
         ylim = [1e0, 1e4]
     end
     plt = plot(yaxis=:log,
-               xlim=[0, 900],
-               ylim=ylim,
+               #xlim=[0, 2000],
+               #ylim=ylim,
                xlabel="function evaluations",
                ylabel=L"\max\{R_p,R_d\}",
                size=(900,600))
@@ -62,23 +66,22 @@ function makeplot(r1, r2)
 end
 
 
-function test(n, γ, kmax; f_calls_limit=2000)
-    r1, r2 = runtests(n, γ, kmax, f_calls_limit)
+function test(n, γ; f_calls_limit=2000)
+    r1, r2 = runtests(n, γ, f_calls_limit=f_calls_limit)
     plt = makeplot(r1, r2)
-    savefig(plt, "../figs/n$n-γ$γ-kmax$kmax.pdf")
+    savefig(plt, "../figs/n$n-γ$γ.pdf")
     return nothing
 end
 
-############################################################
+################################################################################
 
 
-@printf("%4s %6s %6s %8s %8s %10s %8s\n",
-        "n", "γ", "kmax", "method", "fgcalls", "resval", "time")
+@printf("%4s %6s %8s %8s %10s %10s %16s %8s\n",
+        "n", "γ", "method", "fgcalls", "rp", "rd", "fval", "time")
 
-kmax = 300
 for n = [587, 692, 834, 1255, 1869]
     for γ = [0.1, 0.05]
-        test(n, γ, kmax)
+        test(n, γ)
     end
 end
 
